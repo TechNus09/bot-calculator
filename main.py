cff9600b0de149a926e1080092692df77bcec01f
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2021-10-31 21:26:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ async def selectionTest(ctx,curLv,tarLv):
 
 
 
-@client.event
+@bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(activity=d.Activity(type=d.ActivityType.watching, name="Calculator"))
