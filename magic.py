6e3b44438d06b3e85805eb8f63d2a215b683a625
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2022-01-29 14:06:48 +0100

    Add files via upload

---
+++ magic.py
@@ -0,0 +1,23 @@
+tailoringRsc = [
+    ["paper_ca3",936964378773831721],
+    ["wand_ca3",936964379080024135],
+    ["spellbook_ca3",936964379092594718],
+    ["fire_staff_ca3",936964377867862048],
+    ["nature_staff_ca3",936964379067437096],
+    ["ice_staff_ca3",936964378606075934],
+    ["cursed_staff",936964377964322857],
+    ["icicle_spellbook_ca3",936964378689957958],
+    ["freeze_spellbook",936964378337615912],
+    ["blizzard_spellbook_ca3",936964377712676906],
+    ["leech_spellbook_ca3",936964378891264001],
+    ["drain_spellbook",936964378077564988],
+    ["consume_spellbook_ca3",936964377565876286],
+    ["haunt_spellbook",936964378002071573],
+    ["curse_spellbook_ca3",936964377867874305],
+    ["torture_spellbook_ca3",936964379147124786],
+    ["ember_spellbook",936964378283114506],
+    ["ignite_spellbook_ca3",936964378748674090],
+    ["inferno_spellbook_ca3",936964378568318977]
+    ]
+
+craftingRsc = [["ice_relic",936964378517975040]]
